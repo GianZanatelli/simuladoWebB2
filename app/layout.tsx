854345c94d11766559a140f6dbb27da560eda1ef
Gian Zanatelli <102186006+GianZanatelli@users.noreply.github.com>
2023-11-22 10:58:07 -0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'CalcJoia',
+  description: 'Calcular preço das joias',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+     
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
